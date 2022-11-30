c4c81a69022afb3013d314e7e68da813d32d051d
Leo Georgescu <georgl97@icloud.com>
2022-11-30 14:53:03 -0800

    added a second polybar for the external monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
+killall -q polybar
 # If all your bars have ipc enabled, you can use 
-polybar-msg cmd quit
+#polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+#echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+#polybar top 2>&1 | tee -a /tmp/polybar1.log & disown
 
-echo "Bars launched..."
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+polybar top -c $(dirname $0)/config.ini &
+
+if [[ $(xrandr -q | grep 'HDMI-1 connected') ]]; then
+	polybar external -c $(dirname $0)/config.ini &
+fi
